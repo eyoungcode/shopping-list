44dcb51c1bb98b383473de3dde4f6f7b04c86ccc
Erik Young <youngblkraven@gmail.com>
2019-04-03 10:19:57 -0600

    initial upload shopping list

---
+++ script.js
@@ -0,0 +1,59 @@
+window.onload = function () {
+	initShoppingList();
+};
+
+function initShoppingList () {
+	let form = document.getElementById("item-form");
+
+	form.addEventListener("submit", (event) => {
+		handleItemForm(event, form);
+	});
+}
+function handleItemForm(event, formRef) {
+	if(event.preventDefault){
+		event.preventDefault();
+	}
+addItemToShoppingList();
+	formRef.reset();
+
+
+	return false;
+
+}
+	function addItemToShoppingList () {
+		let itemName = document.getElementById("item-name");
+		let itemAmount = document.getElementById("item-amount");
+		let id = getRandomInt(0, 1000000);
+
+		// create list item html appends to page
+		let itemHtml = createListItemHtml(itemName.value, itemAmount.value, id);
+		let itemListRef = document.getElementById("shopping-list");
+		itemListRef.insertAdjacentHTML("afterend", itemHtml);
+
+		setDeleteButtonEvent(id);
+	}
+	function setDeleteButtonEvent(id){
+	let deleteButton = document.getElementById("button"+id);
+		deleteButton.addEventListener("click", () => {
+
+});
+
+	function createListItemHtml(itemName, itemAmount, id){
+		return `<li id="item${id}">
+		${itemName} - ${itemAmount}
+			<button id="button${id}" types="button">Delete Item</button>
+		</li>`;
+	}
+	function removeListItem(id) {
+		let ListItem = document.getElementById("item"+id);
+		listItem.parentNode.removeChild(listItem);
+
+	}
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}}
+
+
+
