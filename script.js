9a7d87f6a3c09ab60adf77729461ac39fb971806
Erik Young <youngblkraven@gmail.com>
2019-04-03 19:55:39 -0600

    created new shopping list

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 window.onload = function () {
 	initShoppingList();
 };
-
 function initShoppingList () {
 	let form = document.getElementById("item-form");
-
 	form.addEventListener("submit", (event) => {
 		handleItemForm(event, form);
 	});
@@ -15,29 +13,22 @@ function handleItemForm(event, formRef) {
 	}
 addItemToShoppingList();
 	formRef.reset();
-
-
 	return false;
-
 }
 	function addItemToShoppingList () {
 		let itemName = document.getElementById("item-name");
 		let itemAmount = document.getElementById("item-amount");
 		let id = getRandomInt(0, 1000000);
-
 		// create list item html appends to page
 		let itemHtml = createListItemHtml(itemName.value, itemAmount.value, id);
 		let itemListRef = document.getElementById("shopping-list");
 		itemListRef.insertAdjacentHTML("afterend", itemHtml);
-
 		setDeleteButtonEvent(id);
 	}
 	function setDeleteButtonEvent(id){
 	let deleteButton = document.getElementById("button"+id);
 		deleteButton.addEventListener("click", () => {
-
 });
-
 	function createListItemHtml(itemName, itemAmount, id){
 		return `<li id="item${id}">
 		${itemName} - ${itemAmount}
@@ -46,8 +37,7 @@ addItemToShoppingList();
 	}
 	function removeListItem(id) {
 		let ListItem = document.getElementById("item"+id);
-		listItem.parentNode.removeChild(listItem);
-
+		ListItem.parentNode.removeChild(ListItem);
 	}
 function getRandomInt(min, max) {
 	min = Math.ceil(min);
